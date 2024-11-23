167070c5796edc8b3956c4cd636f9ff14aed3a83
Raphaelle Goulong <raphaelle.goulong@gmail.com>
2024-11-23 18:30:58 +0100

    first commit

---
+++ src/pages/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import '../sass/App.scss'
+// import Home from './Home'
+// import Error from './Error'
+// import Header from '../components/Header'
+
+function App() {
+    return (
+        <div> test</div>
+        // <BrowserRouter>
+        //         <>
+
+        //             <Header />
+
+        //             <Routes>
+
+        //                 <Route path="/" element={<Home />} />
+
+        //                 <Route path="*" element={<Error />} />
+        //             </Routes>
+
+        //             <Footer />
+        //         </>
+        //     </BrowserRouter>
+    )
+}
+
+export default App
